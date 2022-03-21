6e4639672b3feb54119699d267d856fc543b7f4e
Pavan <pavan.javali.i@globaldelight.com>
2022-03-21 11:25:04 +0530

    Commit 1

---
+++ internProject/FirstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FirstViewController.swift
+//  internProject
+//
+//  Created by Pavan D J on 18/03/22.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ internProject/SecondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.swift
+//  internProject
+//
+//  Created by Pavan D J on 18/03/22.
+//
+
+import UIKit
+import Firebase
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+        //TODO: Set up a new user in Firebase
+        Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                //success
+                print("Registration Successful!")
+                
+                self.performSegue(withIdentifier: "goToList", sender: self)
+                
+            }
+        }
+    }
+}
